9bd3b726a62b5a192a91124d6b18147ed1408f29
Philipp Kerling <pkerling@casix.org>
2017-06-06 14:02:29 +0200

    Allow enabling VAAPI with Wayland platform

--- cmake/scripts/linux/ArchSetup.cmake
+++ cmake/scripts/linux/ArchSetup.cmake
@@ -44,11 +44,3 @@ endif()
 if(ENABLE_MIR)
   set(ENABLE_VDPAU OFF CACHE BOOL "Disabling VDPAU since no Mir support" FORCE)
 endif()
-
-string(TOLOWER ${CORE_PLATFORM_NAME} CORE_PLATFORM_NAME_LC)
-# Temporary solution until Linux build system and feature configuration is reworked
-if(CORE_PLATFORM_NAME_LC STREQUAL "wayland")
-  message(STATUS "Disabling VDPAU and VA-API due to missing Wayland support")
-  set(ENABLE_VDPAU OFF CACHE BOOL "Disabling VDPAU due to missing Wayland support" FORCE)
-  set(ENABLE_VAAPI OFF CACHE BOOL "Disabling VAAPI due to missing Wayland support" FORCE)
-endif()
